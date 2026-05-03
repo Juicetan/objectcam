83e1691a475944e6f001537157f7fba496836bac
Juicetan <me@justinlam.ca>
2026-05-03 15:45:06 -0400

    Initial app structure;

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './styles/main.scss'
+import javascriptLogo from './assets/javascript.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { setupCounter } from './counter.js'
+
+
+
+setupCounter(document.querySelector('#counter'))
