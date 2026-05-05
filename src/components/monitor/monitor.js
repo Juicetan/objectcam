4b25ac494c9d8c80394078131870a0dd10b92cbf
Juicetan <me@justinlam.ca>
2026-05-05 03:45:29 -0400

    implement initial detection loop logic

--- src/components/monitor/monitor.js
+++ src/components/monitor/monitor.js
@@ -1,22 +1,17 @@
 import { guid } from '@/utils/string.js';
 import { closeMediaStream } from '@/utils/media.js';
+import { getModel, OBJECT_TYPES } from '@/utils/detectionEngine.js';
 
 export class Monitor {
-  static objTypes = {
-    PERSON: 'person',
-    DOG: 'dog',
-    CAT: 'cat',
-    BIRD: 'bird',
-  }
-
   constructor() {
     this.id = guid();
     this.name = `Monitor ${this.id.slice(0, 8)}`;
-    this.objType = Monitor.objTypes.PERSON;
+    this.objType = OBJECT_TYPES.PERSON;
     this.$elem = null;
     this.$video = null;
     this.stream = null;
     this.cameraInfo = null;
+    this.detectionModel = null;
   }
 
   setEl($el){
@@ -46,10 +41,39 @@ export class Monitor {
     });
     this.$video.srcObject = stream;
     this.stream = stream;
+    await this._startDetectionLoop();
+  }
+
+  _stopDetectionLoop(){
+    this.detectionModel = null;
     return this;
   }
 
+  async _startDetectionLoop(){
+    this._stopDetectionLoop();
+    this.detectionModel = await getModel();
+    console.log('detectionModel loaded', this.detectionModel);
+    const detectLoop = async () => {
+      if(!this.detectionModel || !this.$video?.videoWidth){
+        setTimeout(detectLoop, 200);
+        return;
+      }
+      try {
+      const predictions = await this.detectionModel.detect(this.$video, {
+        objType: this.objType
+        });
+        console.log('predictions', predictions);
+        window.requestAnimationFrame(detectLoop);
+      } catch(err){
+        console.error('detection error', err);
+        setTimeout(detectLoop, 200);
+      }
+    }
+    detectLoop();
+  }
+
   stopFeed(){
+    this._stopDetectionLoop();
     if(this.stream){
       closeMediaStream(this.stream);
     }
