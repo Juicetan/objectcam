d5c98f8c7cc2370d50cbfa43eaaa6f3cdb33bbf6
Juicetan <me@justinlam.ca>
2026-05-06 22:30:37 -0400

    Render prediction boxes;

--- src/components/monitor/monitor.js
+++ src/components/monitor/monitor.js
@@ -9,14 +9,17 @@ export class Monitor {
     this.objType = OBJECT_TYPES.PERSON;
     this.$elem = null;
     this.$video = null;
+    this.$vidWrap = null;
     this.stream = null;
     this.cameraInfo = null;
     this.detecting = false;
+    this.predictionBoxes = [];
   }
 
   setEl($el){
     this.$elem = $el;
     this.$video = this.$elem.querySelector('.monitor-feed');
+    this.$vidWrap = this.$elem.querySelector('.canvas-wrap');
     if(this.cameraInfo){
       this.startFeed();
     }
@@ -44,6 +47,16 @@ export class Monitor {
     this._startDetectionLoop();
   }
 
+  stopFeed(){
+    this._stopDetectionLoop();
+    if(this.stream){
+      closeMediaStream(this.stream);
+    }
+    this.$video.srcObject = null;
+    this.stream = null;
+    return this;
+  }
+
   _stopDetectionLoop(){
     this.detecting = false;
     return this;
@@ -66,7 +79,7 @@ export class Monitor {
         const predictions = await detectionModel.detect(this.$video, {
           objType: this.objType
         });
-        console.log('predictions', predictions);
+        this._renderPredictions(predictions);
         window.requestAnimationFrame(detectLoop);
       } catch(err){
         console.error('detection error', err);
@@ -76,15 +89,43 @@ export class Monitor {
     detectLoop();
   }
 
-  stopFeed(){
-    this._stopDetectionLoop();
-    if(this.stream){
-      closeMediaStream(this.stream);
-    }
-    this.$video.srcObject = null;
-    this.stream = null;
-    return this;
+  _renderPredictions(predictions){
+    this._clearPredictions();
+
+    const vidResolution = {
+      width: this.$video.videoWidth,
+      height: this.$video.videoHeight,
+    };
+    const vidWrapDim = this.$vidWrap.getBoundingClientRect();
+    const widthScale = vidWrapDim.width / vidResolution.width;
+    const heightScale = vidWrapDim.height / vidResolution.height;
+
+    predictions.forEach(prediction => {
+      const { objType, confidence, position } = prediction;
+      const { x, y, width, height } = position;
+      const rect = document.createElement('div');
+      rect.classList.add('prediction-box');
+      rect.style.position = 'absolute';
+      rect.style.left = `${x * widthScale}px`;
+      rect.style.top = `${y * heightScale}px`;
+      rect.style.width = `${width * widthScale}px`;
+      rect.style.height = `${height}px`;
+      
+      const label = document.createElement('div');
+      label.classList.add('pb-label');
+      label.innerText = `${objType} ${Math.round(confidence*100)}%`;
+      rect.appendChild(label);
+      
+      this.predictionBoxes.push(rect);
+      this.$vidWrap.appendChild(rect);
+    });
   }
 
-
+  _clearPredictions(){
+    this.predictionBoxes.forEach(box => {
+      box.remove();
+    });
+    this.predictionBoxes = [];
+    return this;
+  }
 }
